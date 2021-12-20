1fac68cffa4da8dd917b2f9420b52f03a2f3b47e
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2021-12-19 22:09:27 -0600

    Added movies

--- index.js
+++ index.js
@@ -4,10 +4,57 @@ const app = express();
 
 app.use(morgan("common"));
 
+let topMovies = [
+  {
+    title: 'Kiki\'s Delivery Service',
+    producer: 'Studio Ghibli'
+  },
+  {
+    title: 'Princess Mononoke',
+    producer: 'Studio Ghibli'
+  }
+  {
+    title: 'Ponyo',
+    producer: 'Studio Ghibli'
+  }
+  {
+    title: 'Howl\'s Moving Castle',
+    producer: 'Studio Ghibli'
+  }
+  {
+    title: 'Spirited Away',
+    producer: 'Studio Ghibli'
+  }
+  {
+    title: 'Caslte In the Sky',
+    producer: 'Studio Ghibli'
+  }
+  {
+    title: 'My Neighbor Totoro',
+    producer: 'Studio Ghibli'
+  }
+  {
+    title: 'Nausicaa of the Valley of the Wind',
+    producer: 'Studio Ghibli'
+  }
+  {
+    title: 'The Wind Rises',
+    producer: 'Studio Ghibli'
+  }
+  {
+    title: 'When Marnie Was There',
+    producer: 'Studio Ghibli'
+  }
+]
+
 app.get("/", (req, res) => {
   res.send("Welcome to myFlix!");
 });
 
-app.get("/secreturl", (req, res) => {
-  res.send("This is a secret url with very top-secret content.");
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
 });
