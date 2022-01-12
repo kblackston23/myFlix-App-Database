59a226d5660fc13699065b38b16e3d2db15a13d1
kblackston23 <91281323+kblackston23@users.noreply.github.com>
2022-01-11 19:44:30 -0600

    added cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 const Models = require("./models.js");
+const cors = require("cors");
 
 const myFlixDB = Models.Movie;
 const Users = Models.User;
@@ -12,6 +13,23 @@ mongoose.connect("mongodb://localhost:27017/myFlixDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+//cors
+let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
 
 //morgan
 app.use(morgan("common"));
